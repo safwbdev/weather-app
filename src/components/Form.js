d52dff1615e70b7d56ee414af577e5ad387b353c
Stefaan <safwebdev@gmail.com>
2019-10-01 09:54:07 +0800

    remove commented classes

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,4 @@
-import React, { Component } from 'react'
-
-// class Form extends Component {
-//     render() {
-//         return (
-//             <form onSubmit={this.props.getWeather}>
-//                 <input type="text" name="city" placeholder="City" />
-//                 <input type="text" name="country" placeholder="Country" />
-//                 <button>Get Weather</button>
-//             </form>
-//         )
-//     }
-// };
+import React from 'react'
 
 const Form = props => (
     <form onSubmit={props.getWeather}>
--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,21 +1,10 @@
 import React from 'react';
 
-// class Titles extends React.Component{
-//     render() {
-//         return(
-//             <div>
-//                 <h1>Weather Finder</h1>
-//                 <p>Find out Temperature, conditiond and more...</p>
-//             </div>
-//         );
-//     }
-// };
-
 const Titles = () => (
     <div>
-                 <h1>Weather Finder</h1>
-                 <p>Find out Temperature, conditiond and more...</p>
-             </div>
+        <h1>Weather Finder</h1>
+        <p>Find out Temperature, conditiond and more...</p>
+    </div>
 )
 
 
